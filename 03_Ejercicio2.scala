c76d9cca3a2a05d85c14c181d5911a0218e45718
Israel Rivera <israel.96rg@hotmail.com>
2015-09-17 20:18:39 -0500

    Ejercicio 2, practica 3

---
+++ 03_Ejercicio2.scala
@@ -0,0 +1,18 @@
+println("Edad de la persona adscrita a jubilarse")
+val Edad=readInt()
+println("Antiguedad en su empleo de la persona adscrita a jubilarse")
+val Antiguedad=readInt()
+ if (Edad>= 60 && Antiguedad < 25){
+ 	println("El adscrito se jubilara por edad")
+ } 
+ else if(Edad < 60 && Antiguedad >= 25){
+ 	println("El adscrito se jubilara por antiguedad joven ")
+ 	}
+ 	else if (Edad>= 60 && Antiguedad >= 25) {
+ 		println("usted se jubilara por antiguedad adulta")
+
+ 	} 
+ 	
+
+ 	
+ 
