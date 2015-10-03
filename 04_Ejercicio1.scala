110b425765dd8510088df9385c411fabf4bfbc86
Israel Rivera <israel.96rg@hotmail.com>
2015-10-03 00:05:52 -0500

    Ejercicio 1, practica 4

---
+++ 04_Ejercicio1.scala
@@ -0,0 +1,21 @@
+var tiempo=0.0
+var promedio=0.0
+
+for( i <- 1 to 10) {
+	print("ingresa el tiempo recorrido del dia " + (i) + ": ")
+	tiempo=readFloat
+	promedio+=(tiempo)/10
+	}
+	
+	println("su promedio es: " + promedio)
+	if( promedio > 15){
+		print("no cumples los requisitos, debes cambiar de especialidad ")
+		
+	  
+	}
+	else{
+		print("eres apto  para la prueba de 5km ")
+	}
+  
+	
+	
